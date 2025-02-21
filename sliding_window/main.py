03efb1ac05921fad98f097df5af9d8c22eb6a76c
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2025-02-21 11:54:42 +0000

    Implementing acknowledgements

--- sliding_window/main.py
+++ sliding_window/main.py
@@ -2,11 +2,15 @@ import subprocess
 
 # Runs the different python files at the same time
 if __name__ == "__main__":
+
+    # Choose the task to run
+    task = 2
+
     # Start the receiver
-    receiver = subprocess.Popen(["python3", "Receiver1.py", "12345", "received.jpg"])
+    receiver = subprocess.Popen(["python3", "Receiver{}.py".format(task), "12345", "received.jpg"])
 
     # Start the sender
-    sender = subprocess.Popen(["python3", "Sender1.py", "localhost", "12345", "test.jpg"])
+    sender = subprocess.Popen(["python3", "Sender{}.py".format(task), "localhost", "12345", "test.jpg"])
 
     # Wait for both processes to complete
     receiver.wait()
